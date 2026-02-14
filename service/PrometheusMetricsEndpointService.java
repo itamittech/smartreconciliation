977f963df914e127fee7937ef421f3259f960985
Amit Mishra <39621726+itamittech@users.noreply.github.com>
2026-02-14 14:18:38 +0530

    feat: add service/PrometheusMetricsEndpointService.java

---
+++ service/PrometheusMetricsEndpointService.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PrometheusMetricsEndpointService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrometheusMetricsEndpointService.class);
+
+    // Service for Prometheus metrics endpoint
+    public void execute() {
+        LOGGER.info("Executing service logic");
+    }
+}

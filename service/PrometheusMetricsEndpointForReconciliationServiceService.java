99c1e882dd8b48246752ee7099c9c1584a58b46c
Amit Mishra <39621726+itamittech@users.noreply.github.com>
2026-02-14 14:17:20 +0530

    feat: add service/PrometheusMetricsEndpointForReconciliationServiceService.java

---
+++ service/PrometheusMetricsEndpointForReconciliationServiceService.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PrometheusMetricsEndpointForReconciliationServiceService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrometheusMetricsEndpointForReconciliationServiceService.class);
+
+    // Service for Prometheus metrics endpoint for reconciliation service
+    public void execute() {
+        LOGGER.info("Executing service logic");
+    }
+}

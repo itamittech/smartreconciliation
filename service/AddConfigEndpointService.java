3a2a63ebf8eb48073830098e90f16d086567bd9b
Amit Mishra <39621726+itamittech@users.noreply.github.com>
2026-02-14 15:28:21 +0530

    feat: add service/AddConfigEndpointService.java

---
+++ service/AddConfigEndpointService.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AddConfigEndpointService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AddConfigEndpointService.class);
+
+    // Service for Add /config endpoint
+    public void execute() {
+        LOGGER.info("Executing service logic");
+    }
+}
